edd28af8ea86713ec9c3c20c586716b97e05e2a9
Admin inc <107643055+administrati0n@users.noreply.github.com>
2023-07-16 18:24:15 +0300

    Update data1.js

--- data1.js
+++ data1.js
@@ -21,14 +21,14 @@ var products1 = [
 
 var products2 = [
   { 
-  catlogo: "url('xcodel.png')", 
-  add1: "url('firebbasel.png')", 
-  add2: "url('xcodepack.png')", 
+  catlogo: "url('https://administrati0n.github.io/data-one/xcodel.png')", 
+  add1: "url('https://administrati0n.github.io/data-one/firebbasel.png')", 
+  add2: "url('https://administrati0n.github.io/data-one/xcodepack.png')", 
   accentcolor: "rgba(33, 172, 222, 1.0)", 
   accentcolor1: "rgb(5, 28, 34)", 
   accentcolor2: "rgb(12, 72, 94)", 
-  catlogo1: "url('appstorel.png')", 
-  logoproj: "url('Starter Pack.png')", 
+  catlogo1: "url('https://administrati0n.github.io/data-one/appstorel.png')", 
+  logoproj: "url('https://administrati0n.github.io/data-one/Starter Pack.png')", 
   название: "Starter Pack", 
   описание: "Описание 6", 
   leftT: "Xcode", 
@@ -38,14 +38,14 @@ var products2 = [
   data0: "2023-07-05"
 },
   { 
-  catlogo: "url('xcodel.png')", 
-  add1: "url('firebbasel.png')", 
-  add2: "url('xcodepack.png')", 
+  catlogo: "url('https://administrati0n.github.io/data-one/xcodel.png')", 
+  add1: "url('https://administrati0n.github.io/data-one/firebbasel.png')", 
+  add2: "url('https://administrati0n.github.io/data-one/xcodepack.png')", 
   accentcolor: "rgba(33, 172, 222, 1.0)", 
   accentcolor1: "rgb(5, 28, 34)", 
   accentcolor2: "rgb(12, 72, 94)", 
-  catlogo1: "url('appstorel.png')", 
-  logoproj: "url('Starter Pack.png')", 
+  catlogo1: "url('https://administrati0n.github.io/data-one/appstorel.png')", 
+  logoproj: "url('https://administrati0n.github.io/data-one/Starter Pack.png')", 
   название: "Starter Pack", 
   описание: "Описание 6", 
   leftT: "Xcode", 
@@ -55,9 +55,9 @@ var products2 = [
   data0: "2023-07-02"
 },
   { 
-  catlogo: "url('xcodel.png')", 
-  add1: "url('firebbasel.png')", 
-  add2: "url('xcodepack.png')", 
+  catlogo: "url('https://administrati0n.github.io/data-one/xcodel.png')", 
+  add1: "url('https://administrati0n.github.io/data-one/firebbasel.png')", 
+  add2: "url('https://administrati0n.github.io/data-one/xcodepack.png')", 
   accentcolor: "rgba(33, 172, 222, 1.0)", 
   accentcolor1: "rgb(5, 28, 34)", 
   accentcolor2: "rgb(12, 72, 94)", 
