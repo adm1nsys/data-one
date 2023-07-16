72b9977f5a53a4235f01c77795408ebb96bdfb56
Admin inc <107643055+administrati0n@users.noreply.github.com>
2023-07-16 17:59:03 +0300

    Update Content.js

--- Content.js
+++ Content.js
@@ -1,21 +1,21 @@
 // consoledev.js
 const consoledev = document.createElement("script");
-consoledev.src = "consoledev.js";
+consoledev.src = "https://administrati0n.github.io/data-one/consoledev.js";
 document.body.appendChild(consoledev);
 
 // Container0.js
 const Container00 = document.createElement("script");
-Container00.src = "Container0.js";
+Container00.src = "https://administrati0n.github.io/data-one/Container0.js";
 document.body.appendChild(Container00);
 
 // Container1.js
 const Container11 = document.createElement("script");
-Container11.src = "Container1.js";
+Container11.src = "https://administrati0n.github.io/data-one/Container1.js";
 document.body.appendChild(Container11);
 
 // ContainerForContent.js
 const ContainerForContent = document.createElement("script");
-ContainerForContent.src = "ContainerForContent.js";
+ContainerForContent.src = "https://administrati0n.github.io/data-one/ContainerForContent.js";
 document.body.appendChild(ContainerForContent);
 
 
