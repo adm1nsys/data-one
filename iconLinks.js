b7017878956fb9d19f8e21908e0fd6deefcdc3b2
Admin inc <107643055+administrati0n@users.noreply.github.com>
2023-08-26 02:33:30 +0300

    Delete iconLinks.js

--- iconLinks.js
+++
@@ -1,9 +0,0 @@
-const iconLinks = `
-  <link rel="shortcut icon" href="img/logoico1.ico" type="image/x-icon">
-  <link rel="icon" href="img/logoico32.png" sizes="32x32">
-  <link rel="icon" href="img/logoico48.png" sizes="48x48">
-  <link rel="icon" href="img/logoico96.png" sizes="96x96">
-  <link rel="icon" href="img/logoico144.png" sizes="144x144">
-`;
-
-document.head.insertAdjacentHTML('beforeend', iconLinks);
