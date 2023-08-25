f07b1ba9ca4fba4ac2977831fd445721510cb683
Admin inc <107643055+administrati0n@users.noreply.github.com>
2023-08-26 00:43:11 +0300

    Add files via upload

--- lib.js
+++ lib.js
@@ -1,12 +1,12 @@
 const jqueryScript = document.createElement("script");
-jqueryScript.src = BASE_URL + "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js";
+jqueryScript.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js";
 document.head.appendChild(jqueryScript);
 
 const prismCSS = document.createElement("link");
 prismCSS.rel = "stylesheet";
-prismCSS.href = BASE_URL + "https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/themes/prism.min.css";
+prismCSS.href = "https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/themes/prism.min.css";
 document.head.appendChild(prismCSS);
 
 const prismScript = document.createElement("script");
-prismScript.src = BASE_URL + "https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/prism.min.js";
+prismScript.src = "https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/prism.min.js";
 document.head.appendChild(prismScript);
