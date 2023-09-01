24d9879ab8bd3d818d7267f568aaae04f2e5f395
Admin inc <107643055+administrati0n@users.noreply.github.com>
2023-09-01 12:54:37 +0300

    Add files via upload

--- Content.js
+++ Content.js
@@ -1,6 +1,12 @@
 loadScript('consoledev');
 loadScript('Container0');
-loadScript('Container1');
+const container01 = document.createElement("script");
+
+container01.src = "https://administrati0n.github.io/data-one/Container1.js";
+
+document.body.appendChild(container01);
+
+// loadScript('Container1');
 loadScript('ContainerForContent');
 
 
